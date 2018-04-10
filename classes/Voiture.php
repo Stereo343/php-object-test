2e1d49ded7d0eab9bae4d4dd9b7f5581cb315e21
Stereo343 <brunocornet343@gmail.com>
2018-04-10 09:36:53 +0200

    dev: add calcul in index.php and calculSphere calculVolume in sphere.php

---
+++ classes/Voiture.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Stereo
+ * Date: 10/04/2018
+ * Time: 09:31
+ */
+
+class Voiture
+{
+    public $couleur;
+    public $masse;
+    public $marque;
+    public $puissance;
+}
